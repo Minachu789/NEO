b47d0394bb0f4485944c2535a7ae5298807790f5
Mina <minachu3344@gmail.com>
2024-12-12 19:59:14 +0800

    Add files via upload

--- src/main/java/frc/robot/subsystems/HopperSubsystem.java
+++ src/main/java/frc/robot/subsystems/HopperSubsystem.java
@@ -2,9 +2,10 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class IntakeSubsystem extends SubsystemBase {
+public class HopperSubsystem extends SubsystemBase {
     private final CANSparkMax motor = new CANSparkMax(0, MotorType.kBrushless);
 
     public void move(double speed) {
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,17 +1,17 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
-
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
     private final CANSparkMax motor = new CANSparkMax(0, MotorType.kBrushless);
+
     public void move(double speed) {
         this.motor.set(speed);
     }
-    public void stop(){
+
+    public void stop() {
         this.motor.stopMotor();
     }
 }
-
